a3b83ed370087a238b59b78c59ece4ab3633eccb
David Rodriguez <rodriguezd11@uabc.edu.mx>
2022-04-08 11:59:29 -0700

    Agregar archivos de proyecto.

---
+++ CicloFor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i, numAlumnos;
+            double calificacion, sumaCalif = 0, promedio;
+
+            Console.Write("Ingresa el numero de alumnos: ");
+            numAlumnos = Convert.ToByte(Console.ReadLine());
+
+            for (i=1; i <= numAlumnos; i++)
+            {
+                Console.Write("Ingresa la calificacion: ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+
+                sumaCalif += calificacion;
+            }
+
+            promedio = sumaCalif / numAlumnos;
+
+
+            //Mostramos el promedio
+            Console.WriteLine("El promedio  es: {0}", promedio);
+            Console.ReadKey();
+
+        }
+    }
+}
